cc42edfd166c3dcddc89468e90249aa7c32231ad
XJ6 um sonho <luquinhaplayx@gmail.com>
2018-07-20 15:56:03 -0300

    Update MainActivity.java

--- app/src/main/java/planks/xschool/com/xschoolplanks/MainActivity.java
+++ app/src/main/java/planks/xschool/com/xschoolplanks/MainActivity.java
@@ -2,12 +2,24 @@ package planks.xschool.com.xschoolplanks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.buttonId);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Deu certo", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
