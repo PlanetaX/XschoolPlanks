99e50408a408f24e18f720942277b670fc55f039
PlanetaX <luquinhaplayx@gmail.com>
2018-07-23 12:58:49 -0300

    Merge remote-tracking branch 'githubOrigin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/planks/xschool/com/xschoolplanks/MainActivity.java
+++ app/src/main/java/planks/xschool/com/xschoolplanks/MainActivity.java
@@ -6,20 +6,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
-    private Button btn;//test
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = (Button) findViewById(R.id.buttonId);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Deu certo", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
