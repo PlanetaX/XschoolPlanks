7f46f48642b823d7eb5af775b5174ff9966e3bac
PlanetaX <luquinhaplayx@gmail.com>
2018-07-23 14:31:35 -0300

    Merge remote-tracking branch 'githubOrigin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/planks/xschool/com/xschoolplanks/MainActivity.java
+++ app/src/main/java/planks/xschool/com/xschoolplanks/MainActivity.java
@@ -6,14 +6,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference userRef  = reference.child("users");
     private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String nome = "";
+
+        userRef.child("Nome").setValue(nome);
 
     }
 }
---
+++ app/src/main/java/planks/xschool/com/xschoolplanks/model/User.java
@@ -0,0 +1,43 @@
+package planks.xschool.com.xschoolplanks.model;
+
+public class User {
+    private String userName;
+    private String userId;
+    private String userPassword;
+    private int userPhone;
+
+    public User() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public int getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(int userPhone) {
+        this.userPhone = userPhone;
+    }
+}
