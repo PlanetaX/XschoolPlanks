d7cef2ea202440a4f18b9d7f48307a64aa0de157
PlanetaX <luquinhaplayx@gmail.com>
2018-07-23 14:38:19 -0300

    Merge remote-tracking branch 'githubOrigin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/planks/xschool/com/xschoolplanks/MainActivity.java
+++ app/src/main/java/planks/xschool/com/xschoolplanks/MainActivity.java
@@ -9,18 +9,23 @@ import android.widget.Toast;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import planks.xschool.com.xschoolplanks.model.User;
+
 public class MainActivity extends AppCompatActivity {
     private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
-    private DatabaseReference userRef  = reference.child("users");
+    private DatabaseReference userRef  = reference.child("Database").child("Users");
     private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String nome = "";
+        User user = new User();
+        String nome = user.getUserName();
 
-        userRef.child("Nome").setValue(nome);
+        nome = "Luiz";
+
+        userRef.child("Usuários").setValue(nome);
 
     }
 }
