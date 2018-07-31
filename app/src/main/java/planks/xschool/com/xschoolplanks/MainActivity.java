2478060c26bcbb5577ab4a5633c73020016a3db5
PlanetaX <luquinhaplayx@gmail.com>
2018-07-31 13:36:53 -0300

    commit

--- app/src/main/java/planks/xschool/com/xschoolplanks/MainActivity.java
+++ app/src/main/java/planks/xschool/com/xschoolplanks/MainActivity.java
@@ -1,5 +1,7 @@
 package planks.xschool.com.xschoolplanks;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,12 +26,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //FRAGMENTO EXEMPLO
+        /*
+        login = (Button) findViewById(R.id.btn_logar);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                if(status) {
+                    LoginFragment loginFragment = new LoginFragment();
+                    fragmentTransaction.add(R.id.rl_container_fragmento, loginFragment);
+                    fragmentTransaction.commit();
+                    login.setText("Cadastre-se");
+                    status = false;
+                }else {
+                    CadastroFragment cadastroFragment = new CadastroFragment();
+                    fragmentTransaction.add(R.id.rl_container_fragmento, cadastroFragment);
+                    fragmentTransaction.commit();
+                    login.setText("Logar");
+                    status = true;
+                }
+            }
+        });
+        */
+
+
+        //FIREBASE EXAMPLE
         /*
         User user = new User();
         user.setUserName("Anyone");
         String nome = user.getUserName();
         userRef.child("Usuários").setValue(nome);
         */
+
+
 
     }
 }
