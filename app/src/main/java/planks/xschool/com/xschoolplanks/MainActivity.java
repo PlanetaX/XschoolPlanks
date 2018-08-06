5f41c8f6c18092072dc41645abbf81c0d19ab0ed
PlanetaX <luquinhaplayx@gmail.com>
2018-08-06 16:50:06 -0300

    06/08 commit

--- app/src/main/java/planks/xschool/com/xschoolplanks/MainActivity.java
+++ app/src/main/java/planks/xschool/com/xschoolplanks/MainActivity.java
@@ -1,6 +1,8 @@
 package planks.xschool.com.xschoolplanks;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
