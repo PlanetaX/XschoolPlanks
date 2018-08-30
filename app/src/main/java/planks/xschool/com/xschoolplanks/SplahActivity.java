5d26f69d45db970ac14341ee9c2e03674b817a92
PlanetaX <luquinhaplayx@gmail.com>
2018-08-30 16:31:17 -0300

    30/08/2018 16:31

--- app/src/main/java/planks/xschool/com/xschoolplanks/SplahActivity.java
+++ app/src/main/java/planks/xschool/com/xschoolplanks/SplahActivity.java
@@ -1,13 +1,27 @@
 package planks.xschool.com.xschoolplanks;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplahActivity extends AppCompatActivity {
+public class SplahActivity extends Activity {
+
+    private static int SPLASH_TIME_OUT = 2000; //Milliseconds
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splah);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
