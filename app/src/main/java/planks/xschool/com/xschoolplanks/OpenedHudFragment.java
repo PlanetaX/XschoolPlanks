38a4ca23f0fd66d83b5ac94339b423a4265ded1f
PlanetaX <luquinhaplayx@gmail.com>
2018-09-01 14:41:03 -0300

    01/09/2018 14:40 //OpenHud

--- app/src/main/java/planks/xschool/com/xschoolplanks/OpenedHudFragment.java
+++ app/src/main/java/planks/xschool/com/xschoolplanks/OpenedHudFragment.java
@@ -1,12 +1,19 @@
 package planks.xschool.com.xschoolplanks;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import planks.xschool.com.xschoolplanks.model.User;
 
 
 /**
@@ -15,6 +22,7 @@ import android.widget.TextView;
 public class OpenedHudFragment extends Fragment {
 
     public TextView texto;
+    RelativeLayout rl;
 
     public OpenedHudFragment() {
         // Required empty public constructor
@@ -25,7 +33,15 @@ public class OpenedHudFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_opened_hud, container, false);
+        View view = inflater.inflate(R.layout.fragment_opened_hud, container, false);
+        texto = (TextView) view.findViewById(R.id.tv_texto);
+
+        texto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+        return view;
     }
 
 }
