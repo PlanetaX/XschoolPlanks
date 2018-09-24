1bde4c2980a553969d9abd0f7d59982ba8e2e378
PlanetaX <luquinhaplayx@gmail.com>
2018-09-24 14:44:47 -0300

    24/09/2018 14:44 //Fatal Error

--- app/src/main/java/planks/xschool/com/xschoolplanks/SplashActivity.java
+++ app/src/main/java/planks/xschool/com/xschoolplanks/SplashActivity.java
@@ -3,7 +3,6 @@ package planks.xschool.com.xschoolplanks;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends Activity {
@@ -15,7 +14,7 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splah);
+        setContentView(R.layout.activity_splash);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
