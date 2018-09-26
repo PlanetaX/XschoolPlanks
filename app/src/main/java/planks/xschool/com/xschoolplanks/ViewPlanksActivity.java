46c38e1361ae5c1075d422532a89d041c506bf78
PlanetaX <luquinhaplayx@gmail.com>
2018-09-26 20:56:11 -0300

    26/09/2018 20:29 //ViewPlanks

---
+++ app/src/main/java/planks/xschool/com/xschoolplanks/ViewPlanksActivity.java
@@ -0,0 +1,14 @@
+package planks.xschool.com.xschoolplanks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewPlanksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_planks);
+        //TODO this
+    }
+}
